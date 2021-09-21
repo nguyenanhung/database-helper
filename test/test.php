8b042df12c5ffdd4a7e6c03f0e1469e2b7a0de43
nguyenanhung <info@nguyenanhung.com>
2021-09-22 01:33:58 +0700

    Test Database Helper Package

--- test/test.php
+++ test/test.php
@@ -7,3 +7,8 @@
  * Date: 09/22/2021
  * Time: 01:22
  */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use nguyenanhung\Bear\Database\Console;
+
+Console::writeLn('Test Database Helper Package');
